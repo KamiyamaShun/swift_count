ffbb59d00bbbec7dec834a7f8b1851f9f773f8f8
KamiyamaShun <76637787+KamiyamaShun@users.noreply.github.com>
2021-02-08 16:50:45 +0900

    set color if

--- swift_count/ViewController.swift
+++ swift_count/ViewController.swift
@@ -11,16 +11,37 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var count_label: UILabel!
     
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        count_label.text = "0"
         
     }
 
     @IBAction func plus(_ sender: Any) {
+        count += 1
+        count_label.text = String(count)
+        
+        if (count >= 10){
+            changeColor()
+        }
     }
     
     @IBAction func minas(_ sender: Any) {
+        count -= 1
+        count_label.text = String(count)
+        if (count < 9) {
+            resetColor()
+        }
     }
     
+    func changeColor() {
+        count_label.textColor = .yellow
+    }
+    
+    func resetColor(){
+        count_label.textColor = .white
+    }
 }
 
